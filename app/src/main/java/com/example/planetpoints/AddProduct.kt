c02227180c450b3ad4393536e8353e64b2aea660
Jaydin_MacBook <74679492+TheManWhoLikesToCode@users.noreply.github.com>
2024-01-27 21:50:13 -0500

    Jaydin

--- app/src/main/java/com/example/planetpoints/AddProduct.kt
+++ app/src/main/java/com/example/planetpoints/AddProduct.kt
@@ -1,11 +1,21 @@
 package com.example.planetpoints
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
+
 
 class AddProduct : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_product)
+
+        val spinnerPackagingType = findViewById<Spinner>(R.id.spinnerPackagingType)
+        val packagingOptions = arrayOf("Paper", "Plastic", "Metal", "Compostable", "Biodegradable")
+        val adapter = ArrayAdapter(this, R.layout.simple_spinner_item, packagingOptions)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerPackagingType.adapter = adapter
     }
 }
