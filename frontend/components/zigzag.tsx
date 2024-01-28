8c529e46b1b13b9fbf9a630a9a32e514a9f407d2
Jaydin_MacBook <74679492+TheManWhoLikesToCode@users.noreply.github.com>
2024-01-28 03:19:25 -0500

    Revert "Added Absolute Paths?"
    This reverts commit aa8f724842bd3976a6349a5d240078d48d3d4baa.

--- frontend/components/zigzag.tsx
+++ frontend/components/zigzag.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image'
 
-import Phone_Demo from './public/images/Phone_Demo.png'
+import Phone_Demo from '@/public/images/Phone_Demo.png'
+import FeatImage01 from '@/public/images/features-03-image-01.png'
+import FeatImage02 from '@/public/images/features-03-image-02.png'
+import FeatImage03 from '@/public/images/features-03-image-03.png'
 
 export default function Zigzag() {
   return (
