c9e2399c740d9bfadbbf7cef16238f6f31744c4b
Jaydin_MacBook <74679492+TheManWhoLikesToCode@users.noreply.github.com>
2024-01-30 13:15:14 -0500

    removed mobile menu

--- frontend/components/ui/mobile-menu.tsx
+++ frontend/components/ui/mobile-menu.tsx
@@ -30,51 +30,5 @@ export default function MobileMenu() {
     return () => document.removeEventListener('keydown', keyHandler)
   })
 
-  return (
-    <div className="md:hidden">
-      {/* Hamburger button */}
-      <button
-        ref={trigger}
-        className={`hamburger ${mobileNavOpen && 'active'}`}
-        aria-controls="mobile-nav"
-        aria-expanded={mobileNavOpen}
-        onClick={() => setMobileNavOpen(!mobileNavOpen)}
-      >
-        <span className="sr-only">Menu</span>
-        <svg
-          className="w-6 h-6 fill-current text-gray-300 hover:text-gray-200 transition duration-150 ease-in-out"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <rect y="4" width="24" height="2" rx="1" />
-          <rect y="11" width="24" height="2" rx="1" />
-          <rect y="18" width="24" height="2" rx="1" />
-        </svg>
-      </button>
-
-      {/*Mobile navigation */}
-      <nav
-        id="mobile-nav"
-        ref={mobileNav}
-        className="absolute top-full z-20 left-0 w-full px-4 sm:px-6 overflow-hidden transition-all duration-300 ease-in-out"
-        style={mobileNavOpen ? { maxHeight: mobileNav.current?.scrollHeight, opacity: 1 } : { maxHeight: 0, opacity: 0.8 }}
-      >
-        <ul className="bg-gray-800 px-4 py-2">
-          <li>
-            <Link href="/signin" className="flex font-medium w-full text-purple-600 hover:text-gray-200 py-2 justify-center" onClick={() => setMobileNavOpen(false)}>
-              Sign in
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="/signup"
-              className="font-medium w-full inline-flex items-center justify-center border border-transparent px-4 py-2 my-2 rounded-sm text-white bg-purple-600 hover:bg-purple-700 transition duration-150 ease-in-out" onClick={() => setMobileNavOpen(false)}
-            >
-              Sign up
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  )
+  return () => {} 
 }
